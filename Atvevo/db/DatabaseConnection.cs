ad5ddf20c5f098a180249d33f491ae382168d584
Misakaiscute <horchy.hunorpihe@gmail.com>
2025-02-21 08:57:33 +0100

    added Sys.Data.SQLite package

---
+++ Atvevo/db/DatabaseConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Atvevo.db
+{
+    public class DatabaseConnection
+    {
+        private string _directory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+        public DatabaseConnection()
+        {
+            
+            //open db
+            //create table if not exist
+            //
+        }
+    } 
+}
