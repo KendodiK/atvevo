742353ff38324453cda2e575af4c1713e38e7032
Misakaiscute <horchy.hunorpihe@gmail.com>
2025-02-21 12:40:28 +0100

    Added database table creations

--- Atvevo/Form1.cs
+++ Atvevo/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Atvevo.db;
 
 namespace Atvevo
 {
@@ -15,6 +9,7 @@ namespace Atvevo
         public Form1()
         {
             InitializeComponent();
+            var databaseConnection = new DatabaseConnection();
         }
     }
 }
--- Atvevo/db/DatabaseConnection.cs
+++ Atvevo/db/DatabaseConnection.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.IO;
 using System.Data.SQLite;
+
 namespace Atvevo.db
 {
     public class DatabaseConnection
     {
-        private const string _DB_NAME = "atvevo.db";
-        private readonly string _connectionString = $"DataSource={_DB_NAME};Version=3;";
-        private SQLiteConnection _connection;
+        private const string DB_NAME = "atvevo.db";
+        private readonly string _connectionString = $"DataSource={DB_NAME};Version=3;";
+        private readonly SQLiteConnection _connection;
         public DatabaseConnection()
         {
             try
@@ -27,14 +27,24 @@ namespace Atvevo.db
         {
             using (var command = new SQLiteCommand(query, _connection))
             {
-                command.ExecuteNonQuery();
+                var affectedRows = command.ExecuteNonQuery();
+                _connection.LogMessage(SQLiteErrorCode.Ok, "Successful query.");
             }
         }
         private void CreateTables()
         {
-            string createSuppliersTable = "CREATE TABLE IF NOT EXISTS suppliers(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, post_code INTEGER NOT NULL, county TEXT NOT NULL, city TEXT NOT NULL, street TEXT NOT NULL, house_number INTEGER NOT NULL, phone INTEGER NOT NULL,supplier_code TEXT NOT NULL)";
+            string createSuppliersTable = 
+                "CREATE TABLE IF NOT EXISTS suppliers(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, post_code INTEGER NOT NULL, county TEXT NOT NULL, city TEXT NOT NULL, street TEXT NOT NULL, house_number INTEGER NOT NULL, phone INTEGER NOT NULL,supplier_code TEXT NOT NULL)";
             Execute(_connection, createSuppliersTable);
-            string createProductsTable = "";  //TODO
+            string createProductsTable = 
+                "CREATE TABLE IF NOT EXISTS products(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, category TEXT NOT NULL, price REAL NOT NULL)"; 
+            Execute(_connection, createProductsTable);
+            string createProductSupplierConnectionsTable =
+                "CREATE TABLE IF NOT EXISTS supplier_product_connection(id INTEGER PRIMARY KEY AUTOINCREMENT, supplier_id INTEGER NOT NULL, product_id INTEGER NOT NULL)";
+            Execute(_connection, createProductSupplierConnectionsTable);
+            string createSupplyArrivailsTable =
+                "CREATE TABLE IF NOT EXISTS supply_arrivals(id INTEGER PRIMARY KEY AUTOINCREMENT, supplier_id INTEGER NOT NULL, product_id INTEGER NOT NULL, arrival_time NUMERIC NOT NULL, quantity INTEGER NOT NULL)";
+            Execute(_connection, createSupplyArrivailsTable);
         }
         public void DatabaseDisconnect()
         {
