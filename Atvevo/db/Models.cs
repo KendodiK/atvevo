48b9f3dbbd3ef4d70eeef83f7f5ce96745314645
Misakaiscute <horchy.hunorpihe@gmail.com>
2025-03-03 11:25:31 +0100

    Added Models.cs

---
+++ Atvevo/db/Models.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Atvevo.db
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ZipCode { get; set; }
+        public string County { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public byte HouseNumber { get; set; }
+        public int Phone { get; set; }
+        public string Code { get; set; }
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public float Price { get; set; }
+    }
+    public class SupplyArrival
+    {
+        public int Id { get; set; }
+        public int SupplierId { get; set; }
+        public int ProductId { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public int Quantity { get; set; }
+    }
+    public class SupplierProductConnection
+    {
+        public int Id { get; set; }
+        public int SupplierId { get; set; }
+        public int ProductId { get; set; }
+    }
+}
