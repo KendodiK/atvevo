71a997174087755ccd7fad2938aa437f6058c84b
Misakaiscute <horchy.hunorpihe@gmail.com>
2025-03-03 12:35:58 +0100

    Added create function for all database tables

--- Atvevo/db/DatabaseConnection.cs
+++ Atvevo/db/DatabaseConnection.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
 using System.Linq;
-using Atvevo.db;
 
-namespace Atvevo.db
-{
-    public class DatabaseConnection
-    {
-        public static readonly string WorkDir =
-            Path.Combine(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", ".."), "db");
+namespace Atvevo.db {
+    public class DatabaseConnection {
+        public static readonly string WorkDir = Path.Combine(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", ".."), "db");
 
         private const string DbName = "Atvevo.db";
         private readonly SQLiteConnection _connection;
@@ -20,15 +16,12 @@ namespace Atvevo.db
         public SupplyArrivalsTable SupplyArrivalsTable;
         public SupplierProductConnectionTable SupplierProductConnectionTable;
 
-        public DatabaseConnection(bool withDummyData = false)
-        {
+        public DatabaseConnection(bool withDummyData = false) {
             string connectionString = DbConnection();
-            try
-            {
+            try {
                 _connection = new SQLiteConnection(connectionString);
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Console.WriteLine(e);
                 throw;
             }
@@ -39,67 +32,46 @@ namespace Atvevo.db
             SupplyArrivalsTable = new SupplyArrivalsTable(this);
             SupplierProductConnectionTable = new SupplierProductConnectionTable(this);
         }
-
-        private string DbConnection()
-        {
-            SQLiteConnectionStringBuilder builder = new SQLiteConnectionStringBuilder
-            {
+        private string DbConnection() {
+            SQLiteConnectionStringBuilder builder = new SQLiteConnectionStringBuilder {
                 DataSource =
                     Path.Combine(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", ".."), "db", DbName),
                 Version = 3
             };
             return builder.ToString();
         }
-
-        public void ExecuteWithoutReturn(string query)
-        {
-            using (var command = new SQLiteCommand(query, _connection))
-            {
+        public void ExecuteWithoutReturn(string query) {
+            using (var command = new SQLiteCommand(query, _connection)) {
                 var affectedRows = command.ExecuteNonQuery();
                 _connection.LogMessage(SQLiteErrorCode.Ok, "Successful query. Affected rows: " + affectedRows);
             }
         }
-
-        public object ExecuteWithSingleReturn(string query)
-        {
-            using (var command = new SQLiteCommand(query, _connection))
-            {
+        public object ExecuteWithSingleReturn(string query) {
+            using (var command = new SQLiteCommand(query, _connection)) {
                 object result = command.ExecuteScalar();
                 _connection.LogMessage(SQLiteErrorCode.Ok, "Successful query.");
                 return result;
             }
         }
-
-        public SQLiteDataReader ExecuteWithMultipleReturn(string query)
-        {
-            using (var command = new SQLiteCommand(query, _connection))
-            {
+        public SQLiteDataReader ExecuteWithMultipleReturn(string query) {
+            using (var command = new SQLiteCommand(query, _connection)) {
                 SQLiteDataReader reader = command.ExecuteReader();
                 return reader;
             }
         }
-
-        public void DatabaseDisconnect()
-        {
+        public void DatabaseDisconnect() {
             _connection.Close();
         }
     }
-
-    public abstract class DatabaseTable<TClass> where TClass : class
-    {
+    public abstract class DatabaseTable<TClass> where TClass : class {
         protected string _tableName;
         protected DatabaseConnection _connection;
-
-        protected void WithDummyData(string dataFilePath)
-        {
-            if (TableEntriesCount() == 0)
-            {
-                using (StreamReader sr = new StreamReader(dataFilePath))
-                {
+        protected void WithDummyData(string dataFilePath) {
+            if (TableEntriesCount() == 0) {
+                using (StreamReader sr = new StreamReader(dataFilePath)) {
                     var headers = sr.ReadLine()?.Split(',');
                     var data = sr.ReadLine();
-                    while (data != null)
-                    {
+                    while (data != null) {
                         var splitData = data.Split(',');
                         Dictionary<string, string> columnsWithValues = headers
                             ?.Zip(splitData, (first, second) => new { first, second })
@@ -119,49 +91,37 @@ namespace Atvevo.db
                 }
             }
         }
-
-        protected int TableEntriesCount()
-        {
+        protected int TableEntriesCount() {
             var result = _connection.ExecuteWithSingleReturn($"SELECT COUNT(*) FROM {_tableName};");
             return Convert.ToInt32(result);
         }
         public abstract TClass[] Read();
+        public abstract bool Create(TClass model);
     }
-
-    public class SuppliersTable : DatabaseTable<Supplier>
-    {
+    public class SuppliersTable : DatabaseTable<Supplier> {
         private const string TableName = "suppliers";
-
-        public SuppliersTable(DatabaseConnection connection, bool withDummyData = false)
-        {
+        public SuppliersTable(DatabaseConnection connection, bool withDummyData = false) {
             _connection = connection;
             _tableName = TableName;
             string createSuppliersTable =
                 $"CREATE TABLE IF NOT EXISTS {TableName} (id INTEGER PRIMARY KEY, name TEXT NOT NULL, post_code INTEGER NOT NULL, county TEXT NOT NULL, city TEXT NOT NULL, street TEXT NOT NULL, house_number INTEGER NOT NULL, phone INTEGER NOT NULL,supplier_code TEXT NOT NULL);";
             _connection.ExecuteWithoutReturn(createSuppliersTable);
-            if (withDummyData)
-            {
+            if (withDummyData) {
                 WithDummyData(Path.Combine(DatabaseConnection.WorkDir, "besz.csv"));
             }
         }
-
-        public override Supplier[] Read()
-        {
-            if (TableEntriesCount() == 0)
-            {
+        public override Supplier[] Read() {
+            if (TableEntriesCount() == 0) {
                 return Array.Empty<Supplier>();
             }
             List<Supplier> result = new List<Supplier>();
-            var queryResult = _connection.ExecuteWithMultipleReturn($"SELECT * FROM {TableName}");
-            while (queryResult.Read())
-            {
+            var queryResult = _connection.ExecuteWithMultipleReturn($"SELECT * FROM {TableName};");
+            while (queryResult.Read()) {
                 Dictionary<string, string> values = new Dictionary<string, string>();
-                for (int i = 0; i < typeof(Supplier).GetProperties().Length; i++)
-                {
+                for (int i = 0; i < typeof(Supplier).GetProperties().Length; i++) {
                     values.Add(queryResult.GetName(i), queryResult.GetValue(i).ToString());
                 }
-                result.Add(new Supplier
-                {
+                result.Add(new Supplier {
                     Id = Convert.ToInt32(values["id"]),
                     Name = values["name"],
                     City = values["city"],
@@ -175,40 +135,45 @@ namespace Atvevo.db
             }
             return result.ToArray();
         }
-    }
-    public class ProductsTable : DatabaseTable<Product>
-    {
-        private const string TableName = "products";
+        public override bool Create(Supplier model) {
+            string[] keys = {"name", "post_code", "county", "city", "street", "house_number", "phone", "supplier_code"};
+            string[] values = model.GetType().GetProperties().Select(x => x.GetValue(model).ToString()).ToArray();
+            
+            string keysAsString = keys.Aggregate("", (current, key) => current + "," + key).TrimStart(',');
+            string valuesAsString = values.Aggregate("", (current, value) => current + ",'" + value + "'").TrimStart(',');
 
-        public ProductsTable(DatabaseConnection connection, bool withDummyData = false)
-        {
+            try {
+                _connection.ExecuteWithoutReturn($"INSERT INTO {TableName} ({keysAsString}) VALUES({valuesAsString});)");
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
+    }
+    public class ProductsTable : DatabaseTable<Product> {
+        private const string TableName = "products";
+        public ProductsTable(DatabaseConnection connection, bool withDummyData = false) {
             _connection = connection;
             _tableName = TableName;
             string createSuppliersTable =
                 $"CREATE TABLE IF NOT EXISTS {TableName} (id INTEGER PRIMARY KEY, name TEXT NOT NULL, category TEXT NOT NULL, price REAL NOT NULL);";
             _connection.ExecuteWithoutReturn(createSuppliersTable);
-            if (withDummyData)
-            {
+            if (withDummyData) {
                 WithDummyData(Path.Combine(DatabaseConnection.WorkDir, "gyumolcs.csv"));
             }
         }
-        public override Product[] Read()
-        {
-            if (TableEntriesCount() == 0)
-            {
+        public override Product[] Read() {
+            if (TableEntriesCount() == 0) {
                 return Array.Empty<Product>();
             }
             List<Product> result = new List<Product>();
-            var queryResult = _connection.ExecuteWithMultipleReturn($"SELECT * FROM {TableName}");
-            while (queryResult.Read())
-            {
+            var queryResult = _connection.ExecuteWithMultipleReturn($"SELECT * FROM {TableName};");
+            while (queryResult.Read()) {
                 Dictionary<string, string> values = new Dictionary<string, string>();
-                for (int i = 0; i < typeof(Supplier).GetProperties().Length; i++)
-                {
+                for (int i = 0; i < typeof(Supplier).GetProperties().Length; i++) {
                     values.Add(queryResult.GetName(i), queryResult.GetValue(i).ToString());
                 }
-                result.Add(new Product()
-                {
+                result.Add(new Product() {
                     Id = Convert.ToInt32(values["id"]),
                     Name = values["name"],
                     Category = values["category"],
@@ -217,49 +182,80 @@ namespace Atvevo.db
             }
             return result.ToArray();
         }
+        public override bool Create(Product model) {
+            string[] keys = {"name", "category", "price"};
+            string[] values = model.GetType().GetProperties().Select(x => x.GetValue(model).ToString()).ToArray();
+            
+            string keysAsString = keys.Aggregate("", (current, key) => current + "," + key).TrimStart(',');
+            string valuesAsString = values.Aggregate("", (current, value) => current + ",'" + value + "'").TrimStart(',');
+
+            try {
+                _connection.ExecuteWithoutReturn($"INSERT INTO {TableName} ({keysAsString}) VALUES({valuesAsString});)");
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
     }
-
-    public class SupplyArrivalsTable : DatabaseTable<SupplyArrival>
-    {
+    public class SupplyArrivalsTable : DatabaseTable<SupplyArrival> {
         private const string TableName = "supply_arrivals";
-
-        public SupplyArrivalsTable(DatabaseConnection connection, bool withDummyData = false)
-        {
+        public SupplyArrivalsTable(DatabaseConnection connection, bool withDummyData = false) {
             _connection = connection;
             string createSuppliersTable =
                 $"CREATE TABLE IF NOT EXISTS {TableName} (id INTEGER PRIMARY KEY, supplier_id INTEGER NOT NULL, product_id INTEGER NOT NULL, arrival_time NUMERIC NOT NULL, quantity INTEGER NOT NULL);";
             _connection.ExecuteWithoutReturn(createSuppliersTable);
-            if (withDummyData)
-            {
+            if (withDummyData) {
                 WithDummyData(Path.Combine(DatabaseConnection.WorkDir, ""));
             }
         }
-
-        public override SupplyArrival[] Read()
-        {
+        public override SupplyArrival[] Read() {
             return Array.Empty<SupplyArrival>();
         }
+        public override bool Create(SupplyArrival model) {
+            string[] keys = {"supplier_id", "product_id", "arrival_time", "quantity"};
+            string[] values = model.GetType().GetProperties().Select(x => x.GetValue(model).ToString()).ToArray();
+            
+            string keysAsString = keys.Aggregate("", (current, key) => current + "," + key).TrimStart(',');
+            string valuesAsString = values.Aggregate("", (current, value) => current + ",'" + value + "'").TrimStart(',');
+
+            try {
+                _connection.ExecuteWithoutReturn($"INSERT INTO {TableName} ({keysAsString}) VALUES({valuesAsString});)");
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
     }
-
-    public class SupplierProductConnectionTable : DatabaseTable<SupplierProductConnection>
-    {
+    public class SupplierProductConnectionTable : DatabaseTable<SupplierProductConnection> {
         private const string TableName = "supplier_product_connections";
-
-        public SupplierProductConnectionTable(DatabaseConnection connection, bool withDummyData = false)
-        {
+        public SupplierProductConnectionTable(DatabaseConnection connection, bool withDummyData = false) {
             _connection = connection;
             string createSuppliersTable =
                 $"CREATE TABLE IF NOT EXISTS {TableName} (id INTEGER PRIMARY KEY, supplier_id INTEGER NOT NULL, product_id INTEGER NOT NULL);";
             _connection.ExecuteWithoutReturn(createSuppliersTable);
-            if (withDummyData)
-            {
+            if (withDummyData) {
                 WithDummyData(Path.Combine(DatabaseConnection.WorkDir, ""));
             }
         }
-
-        public override SupplierProductConnection[] Read()
-        {
+        public override SupplierProductConnection[] Read() {
             return Array.Empty<SupplierProductConnection>();
+        }
+        public override bool Create(SupplierProductConnection model) {
+            string[] keys = { "supplier_id", "product_id" };
+            string[] values = model.GetType().GetProperties().Select(x => x.GetValue(model).ToString()).ToArray();
+
+            string keysAsString = keys.Aggregate("", (current, key) => current + "," + key).TrimStart(',');
+            string valuesAsString =
+                values.Aggregate("", (current, value) => current + ",'" + value + "'").TrimStart(',');
+
+            try {
+                _connection.ExecuteWithoutReturn(
+                    $"INSERT INTO {TableName} ({keysAsString}) VALUES({valuesAsString});)");
+                return true;
+            }
+            catch (Exception e) {
+                return false;
+            }
         }
     }
 }
