994ca62ae6adf7ff0e5d9e41f2b5757886335b27
LCervus <102804734+KendodiK@users.noreply.github.com>
2025-03-03 11:43:00 +0100

    dropdown

--- Atvevo/MainWindow.cs
+++ Atvevo/MainWindow.cs
@@ -1,14 +1,32 @@
 ﻿using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 using Atvevo.db;
 
 namespace Atvevo
 {
     public partial class MainWindow : Form
     {
+        private ComboBox supplierDropdown;
+
         public MainWindow()
         {
             InitializeComponent();
             var databaseConnection = new DatabaseConnection(true);
+            supplierDropdown = new ComboBox{
+                Parent = this,
+                Width = 100,
+                Height = 20, 
+                Top = 10,
+                Left = 20,
+                DropDownStyle = ComboBoxStyle.DropDownList,
+            };
+
+            string[] suppliers = {"egy", "kettő", "három", "négy"};
+            foreach (var supplier in suppliers)
+            {
+                supplierDropdown.Items.Add(supplier);
+            }
+
             FormClosing += (sender, args) => { databaseConnection.DatabaseDisconnect(); };
         }
     }
